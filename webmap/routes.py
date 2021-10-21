52bf6ed867ef0432d9b9de5ba34c1873481d94a6
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-10-21 14:35:04 +0100

    Add separate template for the map view

--- webmap/routes.py
+++ webmap/routes.py
@@ -1,10 +1,9 @@
 """Route declaration."""
-import folium
 from flask import current_app as app
 from flask import render_template, jsonify, request
-from geopy.geocoders import Nominatim
 
 from webmap.forms import GeocodeLocationForm
+from webmap.mapping import create_map
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -12,34 +11,19 @@ def home():
     form = GeocodeLocationForm()
 
     if request.method == 'POST':
-        return create_map(request.form.get('location'))
+        return render_template(
+            'map.html',
+            title="Flask WebMap Sandbox",
+            map_content=create_map(app, request.form.get('location')),
+        )
 
-    return render_template(
-        'index.html',
-        title="Flask WebMap Sandbox",
-        description="Testing out rendering webmaps with Flask and Folium",
-        form=form,
-    )
-
-
-def create_map(location):
-    # geocode the location
-    geolocator = Nominatim(user_agent=app.name)
-    location = geolocator.geocode(location)
-    point = (location.latitude, location.longitude)
-
-    # create the leaflet map using folium centred on the geocoded point
-    fmap = folium.Map(
-        location=point,
-        zoom_start=14,
-        tiles='cartodbpositron',
-    )
-
-    # add the point to the map
-    popup = folium.Popup(f"{location}<br/>{point}", show=True)
-    folium.Marker(location=point, popup=popup).add_to(fmap)
-
-    return fmap._repr_html_()
+    else:
+        return render_template(
+            'index.html',
+            title="Flask WebMap Sandbox",
+            description="Testing out rendering webmaps with Flask and Folium",
+            form=form,
+        )
 
 
 @app.route("/health")

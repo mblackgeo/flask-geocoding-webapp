7af072d40630e9d285f9e8c4afd0996566b15d35
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-10-21 13:42:24 +0100

    Update run-local environment variables

--- run-local.sh
+++ run-local.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 docker run -ti \
     --net=host \
-    -e ENV='test' \
+    -e SECRET_KEY=verysecure \
+    -e FLASK_APP=flaskwebmapsandbox \
+    -e FLASK_ENV=dev \
     "flask-webmap-sandbox:latest" \
     "$@"

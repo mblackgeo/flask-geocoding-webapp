84b8d57288037d95f5d6ae194bf9217494546e7c
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-10-22 13:05:43 +0100

    Run locally using the .env file

--- run-local.sh
+++ run-local.sh
@@ -3,8 +3,6 @@ set -x
 echo "Running at : http://localhost:5000/"
 docker run -ti \
     --net=host \
-    -e SECRET_KEY=verysecure \
-    -e FLASK_APP=flaskwebmapsandbox \
-    -e FLASK_ENV=dev \
+    --env-file ./.env \
     "flask-webmap-sandbox:latest" \
     "$@"

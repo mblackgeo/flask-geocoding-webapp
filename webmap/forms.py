dd45168092975f2e69417ce39a4470d5cbe45e18
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-10-22 13:04:18 +0100

    Add a select field for changing the geocoding provider

--- webmap/forms.py
+++ webmap/forms.py
@@ -1,9 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import SelectField, StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class GeocodeLocationForm(FlaskForm):
-    """Contact form."""
     location = StringField('Enter location to geocode using Nominatim', [DataRequired()])
+    provider = SelectField(
+        'Select geocoding provider',
+        choices=[('mapbox', 'Mapbox'), ('osm', 'OpenStreetMap Nominatim')],
+    )
     submit = SubmitField('Submit')

bf64c56a40966eee432705f721e7877b2a350a5d
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-10-25 15:17:39 +0100

    Add skeleton setup file so package is installable

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='geocoderapp',
+    version='0.0.1',
+    url='https://github.com/mblackgeo/flask-geocoding-webapp.git',
+    author='Martin Black',
+    author_email='mblack@sparkgeo.com',
+    description='A simple geocoding webapp using Flask and Folium',
+    packages=find_packages(),
+    install_requires=[],  # TODO
+)

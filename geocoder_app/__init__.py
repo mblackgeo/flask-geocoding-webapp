6841565d2da4fcba18f8dbbc5281be3da012c0b9
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-10-25 15:59:28 +0100

    Move configuration file and update import

--- geocoder_app/__init__.py
+++ geocoder_app/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 def create_app():
     """Construct the core application."""
     app = Flask(__name__)
-    app.config.from_object("config.Config")
+    app.config.from_object("geocoder_app.config.Config")
 
     with app.app_context():
         from . import routes  # noqa: F401

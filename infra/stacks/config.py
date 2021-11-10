f89b8d095df319653182efbb4ab56946d277ecc6
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-11-10 15:50:15 +0000

    Bump defaults

--- infra/stacks/config.py
+++ infra/stacks/config.py
@@ -12,7 +12,7 @@ conf = SimpleNamespace(
     aws_account=os.environ["AWS_ACCOUNT"],
     domain_name=os.environ.get("DOMAIN_NAME", ""),
     api_subdomain=os.environ.get("API_SUBDOMAIN", ""),
-    fargate_memory_limit_mb=int(os.environ.get("FARGATE_MEMORY_LIMIT_MB", 512)),
-    fargate_cpu_units=int(os.environ.get("FARGATE_CPU_UNITS", 256)),
+    fargate_cpu_units=int(os.environ.get("FARGATE_CPU_UNITS", 512)),
+    fargate_memory_limit_mb=int(os.environ.get("FARGATE_MEMORY_LIMIT_MB", 1024)),
     port=int(os.environ.get("PORT", 5000)),
 )

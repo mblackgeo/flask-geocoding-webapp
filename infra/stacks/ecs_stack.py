2ad8641b6b1d4083b47ad7d0062b4d536ac575bf
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-11-11 11:14:55 +0000

    Refactor the ecs stack

--- infra/stacks/ecs_stack.py
+++ infra/stacks/ecs_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import aws_certificatemanager as acm
+from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_ecs as ecs
 from aws_cdk import aws_ecs_patterns as ecs_patterns
-from aws_cdk import aws_logs as logs
 from aws_cdk import aws_route53 as route53
 from aws_cdk import core
 
@@ -12,57 +12,47 @@ class ECSStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Register a task definition for this container
-        task_definition = ecs.FargateTaskDefinition(
+        # Create the VPC
+        vpc = ec2.Vpc(self, f"{id}-VPC", max_azs=2)
+
+        # Create the ECS Cluster
+        cluster = ecs.Cluster(self, f"{id}-Cluster", vpc=vpc)
+
+        # Lookup the Route 53 hosted zone for the domain
+        hosted_zone = route53.HostedZone.from_lookup(
             self,
-            id=f"{id}-ecs-task-definition",
-            memory_limit_mib=conf.fargate_memory_limit_mb,
-            cpu=conf.fargate_cpu_units,
+            f"{id}-HostedZone",
+            domain_name=conf.domain_name,
+            private_zone=False,
         )
 
-        # Add the container from the parent directory
-        app_container = task_definition.add_container(
-            id=f"{id}-container",
-            image=ecs.ContainerImage.from_asset(".."),
-            logging=ecs.AwsLogDriver(stream_prefix=id, log_retention=logs.RetentionDays.ONE_DAY),
+        # Create a DNS validated SSL certificate for the loadbalancer
+        certificate = acm.DnsValidatedCertificate(
+            self,
+            f"{id}-Certificate",
+            domain_name=f"{conf.api_subdomain}.{conf.domain_name}",
+            hosted_zone=hosted_zone,
         )
 
-        # Map the container ports
-        app_container.add_port_mappings(ecs.PortMapping(container_port=conf.port, host_port=conf.port))
-
-        # Setup Route 53 domain and certs if required
-        if conf.domain_name:
-            root_domain = conf.domain_name
-            domain_name = f"{conf.api_subdomain}.{root_domain}"
-
-            # Get the HostedZone of the root domain
-            domain_zone = route53.HostedZone.from_lookup(self, "baseZone", domain_name=root_domain)
-
-            # Create a certificate for the api subdomain
-            domain_cert = acm.Certificate(
-                self,
-                "Certificate",
-                domain_name=domain_name,
-                validation=acm.CertificateValidation.from_dns(domain_zone),
-            )
-
-        else:
-            domain_cert = None
-            domain_zone = None
-            domain_name = None
-
-        # Create the load balanced service to serve the container
-        service = ecs_patterns.ApplicationLoadBalancedFargateService(
+        # Use the ApplicationLoadBalancedFargateService construct to pull the local Dockerfile,
+        # push the image to ECR, and deploy to Fargate
+        app = ecs_patterns.ApplicationLoadBalancedFargateService(
             self,
-            id=f"{id}-fargate-service",
+            f"{id}-Webservice",
+            cluster=cluster,
+            domain_name=f"{conf.api_subdomain}.{conf.domain_name}",
+            domain_zone=hosted_zone,
+            certificate=certificate,
             assign_public_ip=True,
+            cpu=conf.fargate_cpu_units,
+            memory_limit_mib=conf.fargate_memory_limit_mb,
             desired_count=1,
-            task_definition=task_definition,
-            open_listener=True,
-            domain_name=domain_name,
-            domain_zone=domain_zone,
-            certificate=domain_cert,
-            enable_ecs_managed_tags=True,
+            task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+                image=ecs.ContainerImage.from_asset(".."),
+                container_port=conf.port,
+                # TODO secrets
+            ),
         )
 
-        service.target_group.configure_health_check(path="/health")
+        # Add a custom health check to the target group in the ApplicationLoadBalancedFargateService constructs
+        app.target_group.configure_health_check(path="/health", port=f"{conf.port}", healthy_http_codes="200,302")
